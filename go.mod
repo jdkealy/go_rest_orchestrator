5cfc3485bf7910b8de347e1afb80f08869b5138e
jdkealy <jdkealy@gmail.com>
2019-10-25 21:55:20 -0400

    lower case / plural

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/jdkealy/go_rails
+
+go 1.13
+
+require github.com/gertd/go-pluralize v0.1.1
--- main.go
+++ main.go
@@ -2,30 +2,61 @@ package main
 
 import (
 	"flag"
+	"log"
 	"os"
+	"github.com/gertd/go-pluralize"
+	"strings"
 	"text/template"
 )
 
 type data struct {
 	Type string
 	Model string
+	PluralModel string
+	PluralLowerModel string
 	Owner string
+	Project string
 	Name string
 }
 
-func main() {
-	var d data
-	flag.StringVar(&d.Owner, "owner", "owner", "Org name")
-	flag.StringVar(&d.Project, "project", "project", "project name")
-	flag.StringVar(&d.Model, "users", "User", "The subtype used for the queue being generated")
-	flag.StringVar(&d.Name, "name", "FOO", "The name used for the queue being generated. This should start with a capital letter so that it is exported.")
-	flag.Parse()
+func cmd(s []string){
+	switch s[1]{
+	case "new": {
+		log.Println("GENERATING NEW PROJECT")
+	}
+	case "scaffold": {
+		scaffold(s)
+	}
+	default: {
+		log.Println("Invalid command. Run help to see list of valid commands")
+	}
+	}
+}
 
-	t := template.Must(template.New("queue").Parse(queueTemplate))
+func scaffold(s []string){
+	pluralize := pluralize.NewClient()
+	modelName := s[2]
+	pluralModelName := pluralize.Plural(modelName)
+	lowercasePluralModelName := strings.ToLower(pluralModelName)
+
+	var d data
+	flag.StringVar(&d.Owner, "owner", "owner", "")
+	flag.StringVar(&d.Project, "project", "project", "")
+	flag.StringVar(&d.Model, "Model", modelName, "")
+	flag.StringVar(&d.PluralModel, "PluralModel", pluralModelName, "")
+	flag.StringVar(&d.PluralLowerModel, "PluralLowerModel", lowercasePluralModelName, "")
+	flag.StringVar(&d.Name, "name", "FOO", "")
+	flag.Parse()
+	t := template.Must(template.New("routes").Parse(routesTemplate))
 	t.Execute(os.Stdout, d)
 }
 
-var queueTemplate = `
+func main() {
+	argsWithProg := os.Args
+	cmd(argsWithProg)
+}
+
+var routesTemplate = `
 package routes
 
 import (
@@ -37,10 +68,9 @@ import (
 )
 
 func (h *Routes) Attach{{.Model}}Routes(r *gin.Engine) {
-	v1 := r.Group("/{{.Model}}")
-	v1.GET("/", h.All{{.Model}})
+	v1 := r.Group("/{{.PluralLowerModel}}")
+	v1.GET("/", h.All{{.PluralModel}})
 	v1.GET("/new", h.New{{.Model}})
 	v1.PUT("/:id", h.Edit{{.Model}})
 	v1.DELETE("/:id", h.Delete{{.Model}})
-
-}
+}`
